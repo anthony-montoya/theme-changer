96fc30cd3674da6394bbb2ab0ff2a5129a0c24da
Anthony Montoya <a.montoya1073@gmail.com>
2017-08-14 12:29:12 -0600

    Finished Theme Changer

--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -9,10 +9,17 @@ export default class SizeChanger extends Component {
   }
 
   // componentWillReceiveProps
+  componentWillReceiveProps(newProps) {
+    this.setState ({
+      allowEdit: newProps.allowEdit
+    })
+  }
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" 
+        onChange= { (event) => this.props.update(parseInt(event.target.value)) }
+        disabled= { this.state.allowEdit === "false" }>
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
